d2571208ebf895db329536d8fe144579bbf3fc00
alexandracch <alexa_theprinces@hotmail.com>
2018-01-09 16:07:24 -0500

    arreglo

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   var config = {
     apiKey: 'AIzaSyBTyPXp0vll8d2Fvi5nViLsKntlNxapEFY',
     authDomain: 'red-social-a1aeb.firebaseapp.com',
-    databaseURL: 'https://red-social-a1aeb.firebaseio.com', 'https://alexandracch.github.io/prueba-social-network/views/login.html', 
+    databaseURL: 'https://red-social-a1aeb.firebaseio.com',
     projectId: 'red-social-a1aeb',
     storageBucket: 'red-social-a1aeb.appspot.com',
     messagingSenderId: '445743781768'
